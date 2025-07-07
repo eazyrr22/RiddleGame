a78e37ebe078615fc3c034a2053ec509af2af397
eazy <ezrirot@gmail.com>
2025-07-07 11:45:11 +0300

    client -models,app (basic game mode,without db)

---
+++ client/app.js
@@ -0,0 +1,20 @@
+import rl, { question } from "readline-sync";
+import Player from "./models/player.js";
+import riddelsArray from "./models/riddles_entities/arrayOfRiddles.js";
+import Round from "./services/round.js";
+
+const answer = rl.question("welcome! what's your name?")
+const currentPlayer = new Player(answer)
+
+for (let rdl of riddelsArray) {
+    Round(rdl, currentPlayer);
+}
+
+const { totalTime, average } = currentPlayer.showStats()
+console.log(`great job  ${answer}! \n total time: ${totalTime} seconds \n average time: ${average} seconds`);
+
+
+
+
+
+
---
+++ client/models/riddle/r1.js
@@ -0,0 +1,12 @@
+import Riddle from "../classes/riddels.js";
+
+const firstRiddle = new Riddle { 
+                    id = 1,
+                    name = "multiply",
+                    taskDescription = "what is 8 * 8?",
+                    correctAnswer = 64
+                    }
+
+
+export default firstRiddle
+
